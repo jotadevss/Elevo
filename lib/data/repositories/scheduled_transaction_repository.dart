610ef528612a8cd68f93a2e986714b22486daeb4
jotadevss <joaopedropereira3232@hotmail.com>
2023-08-17 09:20:16 -0300

    <update> IScheduledTransactionRepository

--- lib/data/repositories/scheduled_transaction_repository.dart
+++ lib/data/repositories/scheduled_transaction_repository.dart
@@ -3,4 +3,5 @@ import 'package:result_dart/result_dart.dart';
 
 abstract class IScheduledTransactionRepository {
   Future<Result<List<ScheduledTransactionDTO>, Exception>> findAllScheduledTransaction(String referenceId);
+  Future<Result<List<ScheduledTransactionDTO>, Exception>> newAppointmentReference(ScheduledTransactionDTO reference);
 }
