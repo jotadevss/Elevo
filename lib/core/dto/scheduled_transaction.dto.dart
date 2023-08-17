bdd044b2e2b1e5f20727ee243aa69774bfe95653
jotadevss <joaopedropereira3232@hotmail.com>
2023-08-17 09:10:48 -0300

    <add> DTO ScheduledTransactio

---
+++ lib/core/dto/scheduled_transaction.dto.dart
@@ -0,0 +1,9 @@
+class ScheduledTransactionDTO {
+  final String referenceId;
+  final String frequencyType;
+
+  ScheduledTransactionDTO({
+    required this.referenceId,
+    required this.frequencyType,
+  });
+}
