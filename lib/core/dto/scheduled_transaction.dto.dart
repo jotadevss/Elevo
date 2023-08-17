a130779449fdef417ec9e62726d363213a97f96e
jotadevss <joaopedropereira3232@hotmail.com>
2023-08-17 10:41:03 -0300

    <add> repo Scheduled

--- lib/core/dto/scheduled_transaction.dto.dart
+++ lib/core/dto/scheduled_transaction.dto.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class ScheduledTransactionDTO {
   final String referenceId;
   final String frequencyType;
@@ -6,4 +9,22 @@ class ScheduledTransactionDTO {
     required this.referenceId,
     required this.frequencyType,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'referenceId': referenceId,
+      'frequencyType': frequencyType,
+    };
+  }
+
+  factory ScheduledTransactionDTO.fromMap(Map<String, dynamic> map) {
+    return ScheduledTransactionDTO(
+      referenceId: map['referenceId'] as String,
+      frequencyType: map['frequencyType'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ScheduledTransactionDTO.fromJson(String source) => ScheduledTransactionDTO.fromMap(json.decode(source) as Map<String, dynamic>);
 }
--- lib/data/repositories/scheduled_transaction_repository.dart
+++ lib/data/repositories/scheduled_transaction_repository.dart
@@ -1,7 +1,45 @@
+import 'dart:developer';
 import 'package:elevo/core/dto/scheduled_transaction.dto.dart';
+import 'package:elevo/core/exception/custom_exception.dart';
+import 'package:elevo/data/sql/sql_config.dart';
+import 'package:elevo/data/sql/sql_helper.dart';
 import 'package:result_dart/result_dart.dart';
 
 abstract class IScheduledTransactionRepository {
-  Future<Result<List<ScheduledTransactionDTO>, Exception>> findAllScheduledTransaction(String referenceId);
-  Future<Result<List<ScheduledTransactionDTO>, Exception>> newAppointmentReference(ScheduledTransactionDTO reference);
+  Future<Result<List<ScheduledTransactionDTO>, Exception>> findAllScheduledTransaction(String frequencyType);
+  Future<Result<ScheduledTransactionDTO, Exception>> newAppointmentReference(ScheduledTransactionDTO reference);
+}
+
+class ScheduledTransactionRepository implements IScheduledTransactionRepository {
+  SQLHelper database = SQLHelper(
+    databaseName: SQLConfig.schedulerDatabaseName,
+    tableName: SQLConfig.schedulerTableName,
+    props: SQLConfig.schedulerProperty,
+  );
+
+  @override
+  Future<Result<List<ScheduledTransactionDTO>, Exception>> findAllScheduledTransaction(String frequencyType) async {
+    try {
+      final data = await database.getByPropQuery('frequencyType', frequencyType);
+      final dto = List.generate(
+        data.length,
+        (i) => ScheduledTransactionDTO.fromMap(data[i]),
+      );
+      return Success(dto);
+    } catch (e) {
+      log(e.toString());
+      return Failure(CustomException());
+    }
+  }
+
+  @override
+  Future<Result<ScheduledTransactionDTO, Exception>> newAppointmentReference(ScheduledTransactionDTO reference) async {
+    try {
+      await database.saveDataQuery(reference.toMap());
+      return Success(reference);
+    } catch (e) {
+      log(e.toString());
+      return Failure(CustomException());
+    }
+  }
 }
