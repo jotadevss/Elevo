d7d5d711fd8727924c5225f5a8b661bc8da34951
jotadevss <joaopedropereira3232@hotmail.com>
2023-08-17 10:27:39 -0300

    <add> categories repo

--- lib/core/entity/category.dart
+++ lib/core/entity/category.dart
@@ -1,12 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class CategoryEntity {
   final String id;
   final String title;
+  final String type;
   final String iconPath;
   final int hexColor;
 
   CategoryEntity({
     required this.id,
     required this.title,
+    required this.type,
     required this.iconPath,
     required this.hexColor,
   });
@@ -14,12 +17,14 @@ class CategoryEntity {
   CategoryEntity copyWith({
     String? id,
     String? title,
+    String? type,
     String? iconPath,
     int? hexColor,
   }) {
     return CategoryEntity(
       id: id ?? this.id,
       title: title ?? this.title,
+      type: type ?? this.type,
       iconPath: iconPath ?? this.iconPath,
       hexColor: hexColor ?? this.hexColor,
     );
--- lib/data/repositories/category_repository.dart
+++ lib/data/repositories/category_repository.dart
@@ -1,6 +1,93 @@
 import 'package:elevo/core/entity/category.dart';
+import 'package:elevo/core/exception/custom_exception.dart';
+import 'package:flutter/material.dart';
 import 'package:result_dart/result_dart.dart';
 
 abstract class ICategoryRepository {
   Future<Result<List<CategoryEntity>, Exception>> getAllCategories();
+}
+
+class CategoryRepository implements ICategoryRepository {
+  @override
+  Future<Result<List<CategoryEntity>, Exception>> getAllCategories() async {
+    try {
+      return Success(_allConstantsCategories);
+    } catch (e) {
+      return Failure(CustomException());
+    }
+  }
+
+  final _allConstantsCategories = <CategoryEntity>[
+    // Initial list of categories
+    CategoryEntity(
+      id: 'content-wage',
+      title: 'Wage',
+      type: "incomes",
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-investment',
+      title: 'incomes Investment',
+      type: 'incomes',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-other-incomes',
+      title: 'Others',
+      type: 'incomes',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-home',
+      title: 'Home',
+      type: 'expenses',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-transport',
+      title: 'Transport',
+      type: 'expenses',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-food',
+      title: 'Food',
+      type: 'expenses',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-health',
+      title: 'Health',
+      type: 'expenses',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-leisure',
+      title: 'Leisure',
+      type: 'expenses',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-taxes',
+      title: 'Taxes',
+      type: 'expenses',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+    CategoryEntity(
+      id: 'content-other-expenses',
+      title: 'Other',
+      type: 'expenses',
+      iconPath: 'car_repair',
+      hexColor: Colors.black.value,
+    ),
+  ];
 }
